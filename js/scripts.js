944aa3ff189ab0caea444ad5f91e38b726ce1533
thatcat13 <thatcat13@gmail.com>
2017-11-02 18:01:52 -0700

    build business logic and frontend logic but variables are not defined in console and current balance display is displaying literally the words objectHTMLinputelement

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 //business logic
 
-function BankAccount(name, initBal) {
-  this.name = name;
-  this.balance = initialBal;
+function BankAccount(newName, newBal) {
+  this.name = newName;
+  this.balance = newBal;
 }
 
 BankAccount.prototype.addMoney = function(depositAmt) {
@@ -14,8 +14,34 @@ BankAccount.prototype.removeMoney = function(withdrAmt) {
 }
 
 
-
-
 //frontend logic
+$(document).ready(function(){
+  $("form#register").submit(function(event){
+    event.preventDefault();
 
-$()
+    var newName = $("#regName").val();
+    var newBal = parseInt($("#newBal").val());
+
+    var newAcct = new BankAccount(newName, newBal);
+    debugger
+  });//event for register
+
+  $("#current").text(newBal);
+
+  $("form#depWithdr").submit(function(event){
+    event.preventDefault();
+
+    var deposit = parseInt($("#deposit").val());
+    var withdrawal = parseInt($("#withdrawal").val());
+
+    if (parseInt($("input#deposit").val()) > 0) {
+      newAcct.addMoney(deposit);
+    } else if (parseInt($("input#deposit").val()) > 0) {
+      newAcct.removeMoney(withdrawal);
+    }
+  });//event for depWithdr
+
+
+    $("#current").text(newAcct.balance);
+
+}); //ready
